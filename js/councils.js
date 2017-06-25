ed2740a6ef8318e0872f7faf31e1c99ceb959df3
anumehaagrawal <anuzenith29@gmail.com>
2017-06-25 06:12:01 +0530

    Councils added , clubs and fests edited

---
+++ js/councils.js
@@ -0,0 +1,61 @@
+  $(document).ready(function(){
+     
+      $("#sidenav").sideNav(); 
+      load();
+      load1();
+         
+    });
+   function get_model(councils){
+          var model=" ";
+          model+='<div class="container container-main">';
+          model+='<div class="councilinfo"><div class="card center-align ">';
+          
+           
+           
+           model+='<div class="card-content orange">'
+          
+          model+='<span class="card-title "><h4><b><i>'+councils.name+' </h3></b></i></span>';
+          model+='<img src="'+councils.image+'"class="responsive-img " style="height:400px;">';
+          model+='<p><b>President :</b><span class="con" >'+councils.president+'</span></p><br/>';
+         
+         model+='<div class="card det councilinfo-bg grey ">';
+         
+        
+           model+='<div class="row">';
+          model+='<div class="container black-text left-align"><p class="flow-text" style="font-size:23px;">'+councils.description+'</p></div><br/>';
+          model+='<div class="container black-text left-align"><span class="majevents"><p class="flow-text" style="font-size:23px;">Major events :</span>'+councils.events+'</p></div><br/>';
+         
+          model+='</div></div></div></div></div>';
+          return model;
+
+        }
+
+        
+        function load(){
+          var council_container=document.getElementById("studentview");
+          var council_row;
+          council_container.innerHTML += '<div id="council-row"></div>';
+          council_row = document.getElementById('council-row');
+          for(var i=0;i<studcouncil.length;i++){
+          var perclub = studcouncil[i];
+         council_row.innerHTML += get_model(perclub);
+
+          }
+        }
+        function load1(){
+          var council_container=document.getElementById("hostelview");
+          var council_row;
+          
+          council_container.innerHTML += '<div id="council-row1"></div>';
+          council_row = document.getElementById('council-row1');
+          for(var i=0;i<hostelcouncil.length;i++){
+       
+       
+          var perclub = hostelcouncil[i];
+         council_row.innerHTML += get_model(perclub);
+
+          }
+
+      
+      }
+        
---
+++ js/fests_main.js
@@ -0,0 +1,48 @@
+
+$(function() {
+  $('#sidenav').sideNav();
+  $('.slider').slider({interval: 3000, height: 450});
+  loadfests();
+});
+
+function loadfests() {
+
+  for (var i = 0; i < fests.length; i++) {
+    if(i%2 === 0) {
+      var row = document.createElement('div');
+      row.className = "row";
+      row.id = "fest-row-" + i/2;
+      row.setAttribute("style","padding-top:30px;");
+      $('#festview').append(row);
+    }
+    fest = fests[i];
+    var holder = document.createElement('div');
+    holder.className = "col s12 m4 offset-m2";
+    holder.setAttribute("style","padding-top:45px;");
+
+    var linkWrapper = document.createElement('a');
+    linkWrapper.setAttribute('href', 'fests.html?id=' + fest.id);
+
+    var container = document.createElement('div');
+    container.className = "fest-card z-depth-3";
+    container.setAttribute("style","width:280px; height:280px;");
+
+    var image = document.createElement('img');
+    image.className = "fest-image";
+    image.src = "img/fests/"+fest.image;
+    image.setAttribute("style","width:280px; height:280px;");
+    image.setAttribute("media","only screen and (max-width : 1024px){.image{height:200px;}}")
+    
+    var title = document.createElement('div');
+    title.className = "fest-title";
+    title.innerText = fest.name;
+    title.setAttribute("style","color:orange; font-size:30px; background: black;")
+
+    container.appendChild(image);
+    container.appendChild(title);
+    linkWrapper.appendChild(container);
+    holder.appendChild(linkWrapper);
+
+    $('#fest-row-'+ parseInt(i/2)).append(holder);
+  }
+}
