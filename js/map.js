dc476f608e16ec78a7764b9912b5791db8f6a31e
salman-bhai <sbs.191197@gmail.com>
2017-07-03 11:39:37 +0530

    Restructure done

---
+++ js/map.js
@@ -0,0 +1,16 @@
+$( document ).ready(function(){
+	$("#sidenav").sideNav();
+	$(document).scroll(function() { 
+		var $nav = $('.navbar-fixed');
+		$nav.find('nav').toggleClass('orange', $(this).scrollTop() > $('.map-page-banner').height());
+		$nav.find('nav').toggleClass('transparent', $(this).scrollTop() < $('.map-page-banner').height());
+    });
+
+	$(".dropdown-button").dropdown();
+    $('.dropdown-button').dropdown({
+      hover: true, // Activate on hover
+      belowOrigin: true, // Displays dropdown below the button
+      alignment: 'left', // Displays dropdown with edge aligned to the left of button
+    }
+  );    
+});
