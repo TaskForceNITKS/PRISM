472acdc6485f363d4073a50820df677852d7fe85
salman-bhai <sbs.191197@gmail.com>
2017-07-03 14:25:27 +0530

    Fests changes added

--- js/fests.js
+++ js/fests.js
@@ -21,13 +21,128 @@ function loadFestInfo(id) {
             fest = fests[i];
         }          
     }
-    $('.header-title').html(fest.name);  
-    $('#content1').html(fest.events);
-    $('#content').html(fest.description); 
-    var image =document.getElementById('festimg');
-    image.src="img/fests/"+fest.image;
-    
 
+    /*
+     * Fests Image Head
+    */
+    var image =document.getElementById('fest-img');
+    image.src="img/fests/"+fest.head_image;
+
+    /*
+     * Fests Title
+    */
+    $('.header-title').html(fest.name);  
+
+
+    /*
+     * Fests Core Team
+    */
+    for (var i = 0; i < fest.team.length; i++) {
+        var member = fest.team[i];
+
+        var container = document.createElement('div');
+        container.className = "member-container col s12 m4";
+
+        var image = document.createElement('img');
+        image.src = "img/fests/" + member.id + "/" + member.head_image;
+        image.className = "circle responsive-img member-image";
+
+        container.appendChild(image);
+
+        var name = document.createElement('div');
+        name.className = "member-name";
+        name.innerText = member.name;
+
+        container.appendChild(name);
+
+        var post = document.createElement('div');
+        post.className = "member-description";
+        post.innerText = member.post_name;
+        post.setAttribute('style', 'text-align:center');
+
+        container.appendChild(description);
+
+        $('#team-details-container').append(container);
+    }
+
+    /*
+     * Fests Introduction
+    */
+    var intro_container = document.getElementById('introduction');
+
+    var intro_image = document.createElement('img');
+    intro_image.src = "img/fests/" + fest.id + "/" + fest.introduction.image;
+    intro_image.className = "responsive-img";
+    intro_container.appendChild(intro_image);
+
+    var intro_content = document.createElement('div');
+    intro_content.className = "intro-content";
+    intro_content.innerText = fest.introduction.content;
+    intro_container.appendChild(intro_content);
+
+    $('#introduction').append(intro_container);
+
+    /*
+     * Fests Committees
+    */
+
+    for (var i = 0; i < fest.committees.length; i++) {
+        var committee = fest.committees[i];
+
+        var committees_container = document.createElement('div');
+        committees_container.className = "committees-container col s12";
+
+        var name = document.createElement('div');
+        name.className = "committee-name";
+        name.innerText = committee.name;
+
+        committees_container.appendChild(name);
+
+        var procedure = document.createElement('div');
+        procedure.className = "committee-procedure";
+        procedure.innerText = committee.procedure;
+
+        container.appendChild(procedure);
+
+        var committee_does = document.createElement('div');
+        committee_does.className = "committee-does";
+        committee_does.innerText = committee.committee_does;
+
+        container.appendChild(committee_does);
+
+
+        $('#committees').append(committees_container);
+    }
+
+    /*
+     * Fests Events
+    */
+    for (var i = 0; i < fest.events.length; i++) {
+        var event = fest.events.team[i];
+
+        var events_container = document.createElement('div');
+        events_container.className = "member-container col s12";
+
+        var image = document.createElement('img');
+        image.src = "img/fests/" + fest.id + "/" + event.image;
+        image.className = "responsive-img";
+
+        events_container.appendChild(image);
+
+        var name = document.createElement('div');
+        name.className = "event-name";
+        name.innerText = event.name;
+
+        events_container.appendChild(name);
+
+        var description = document.createElement('div');
+        description.className = "event-description";
+        description.innerText = event.description;
+
+        events_container.appendChild(description);
+
+        $('#events').append(events_container); 
+    }
 }
 
 
