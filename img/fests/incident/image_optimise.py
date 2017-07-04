4f942a6e3715083541cdac701fb4d0f8ffeca1cb
anumehaagrawal <anuzenith29@gmail.com>
2017-07-05 02:19:12 +0530

    fest json fixed

---
+++ img/fests/incident/image_optimise.py
@@ -0,0 +1,40 @@
+from PIL import Image
+import os
+from os import walk
+from os import listdir
+from os.path import isfile, join
+
+files_list = [f for f in listdir(".") if isfile(join(".", f))]
+print(files_list)
+
+# if len(argv) != 2:
+  #  print("Incorrect usage: %s <filename>" %(argv[0]))
+   # exit()
+
+# file_name = argv[1]
+
+
+for file_name in files_list:
+	name, extension = os.path.splitext(file_name)
+	if extension == '.py':
+		continue
+
+	with Image.open(file_name) as img:
+
+		if extension == '.png': 
+			img = img.convert('RGB')
+			img.save(str(name + '.jpg'))
+			os.system("rm " + file_name)
+			file_name = str(name + '.jpg')
+		width,height = img.size
+		print(str(width) + " " + str(height))
+
+		if height > 300:
+			new_width = ((int)((width*300.0)/height))
+			img = img.resize((500,300), Image.ANTIALIAS)
+
+		# print("Image size after resizing")
+		width,height = img.size
+		# print(str(width) + " " + str(height))
+
+		img.save(file_name, optimize=True, quality=85)
